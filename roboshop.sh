bd3365331da54abb643e961f10b1b46f35c1a9d1
RAVIRAVEEN <raviraveen7@gmail.com>
2023-12-26 22:39:14 +0530

    shell-script

--- roboshop.sh
+++ roboshop.sh
@@ -1,43 +1,38 @@
 #!/bin/bash
+INSTANCES_NAME=("mongodb" "mysql" "redis" "catalogue" "user" "cart" "shipping" "payments" "rabbitmq" "dispatch" "web")
+INSTANCE_TYPE=""
+DOMAIN_PATH="devopsrank.online"
+HOSTED_ZONE_ID="Z05755302BISQRJNR4ESX"
 
-AMI="ami-03265a0778a880afb"
-SG_ID="sg-02deaf6b5f3dd44c0"
-INSTANCES_ID=("mongodb" "mysql" "redis" "cart" "user" "catalogue" "payment" "shipping"  "rabbitmq" "dispatch" "web")
-HOSTED_ID="Z05755302BISQRJNR4ESX"
-DOMAIN_NAME="devopsrank.online"
+for i in "${INSTANCES_NAME[@]}"
+do
+    if [[ $i == "mongodb" || $i == "mysql" || $i == "shipping" ]]; then
+    INSTANCE_TYPE="t2.small"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
 
-
-for i in  "${INSTANCES_ID[0]}"
-do 
-   echo "instance= $1"
-
-   if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
-   then 
-   instance_type="t3.small"
-   else
-   instance_type="t2.micro"
-   fi
-
-   IP_address=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $instance_type --security-group-ids sg-02deaf6b5f3dd44c0 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" --query 'Instance[0], privateIpAddress' --output text)
-
-
- #create r53 record , make sure you delete existing record
-aws route53 change-resource-record-sets \
-  --hosted-zone-id $HOSTED_ID \
-  --change-batch '
-  {
-    "Comment": "Testing creating a record set"
-    ,"Changes": [{
-      "Action"              : "CREATE"
-      ,"ResourceRecordSet"  : {
-        "Name"              : "$i.$DOMAIN_NAME"
-        ,"Type"             : "A"
-        ,"TTL"              : 1
-        ,"ResourceRecords"  : [{
-            "Value"         : "$IP_address"
+    PRIVATE_IPADDRESS=$(aws ec2 run-instances \
+                    --image-id ami-03265a0778a880afb \
+                    --count 1 \
+                    --instance-type "$INSTANCE_TYPE" \
+                    --security-group-ids sg-0f0225d73ad9227cc \
+                    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" \
+                    --query 'Instances[0].PrivateIpAddress' \
+                    --output text)
+    echo "$i : $PRIVATE_IPADDRESS" 
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id "$HOSTED_ZONE_ID" \
+    --change-batch "{
+        \"Changes\": [{
+            \"Action\": \"UPSERT\",
+            \"ResourceRecordSet\": {
+                \"Name\": \"$i.$DOMAIN_PATH\",
+                \"Type\": \"A\",
+                \"TTL\": 1,
+                \"ResourceRecords\": [{\"Value\": \"$PRIVATE_IPADDRESS\"}]
+            }
         }]
-      }
-    }]
-  }
-  ,
-   done
+    }"
+    echo "record created successfully for $i : $i.$DOMAIN_PATH"
+done
